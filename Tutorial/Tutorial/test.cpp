ea063da8b78918a34341ae7b7d49bb53751f2a1b
Vyxhexian <145146179+Vyxhexian@users.noreply.github.com>
2025-07-31 09:18:21 +0300

    triangle with color fluctuation

--- Tutorial/Tutorial/test.cpp
+++ Tutorial/Tutorial/test.cpp
@@ -24,9 +24,10 @@ const char* vertexShaderSource = "#version 330 core\n"
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = ourColor;\n"
 "}\n\0";
 
 
@@ -217,10 +218,20 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT); //state-using function
 
 
+		glUseProgram(shaderProgram);
+
+
+
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f; //changes between 1-0
+
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor"); //get location of ourColor
+
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f); //ourColor location + 4 color value
+
 
 
 		//drawing triangle
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); //each time you're about the draw you need to tell opengl which vao to use. opengl uses global state and only one vao can be activated at a time.
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
